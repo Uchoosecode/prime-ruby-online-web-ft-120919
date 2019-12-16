62c042c83c0818f1991a4faff84ea61639c37757
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-16 01:24:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,17 @@ require 'prime'
 
 
 def prime?(num)
- 
+ if Prime.prime?(num) == true 
+   true
+ else
+   false 
+ end
  
 end
   
 # (2..(num - 1)).none? do |n|
 #   num % n == 0
+  
   
   
 #   end
